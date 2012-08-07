7d7596138680cc2081ff084032df14492b1b857d
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-07 11:21:01 -0400

    Procedure complete.

--- SDI_project2/seago_wesley_assignment2.js
+++ SDI_project2/seago_wesley_assignment2.js
@@ -1,1 +1,47 @@
-alert("JavaScript works!");
+// Wesley Seago
+// SDI 1208
+// Project 2
+// Vampire Hunters
+
+// alert("JavaScript works!");
+
+// declare global variables
+var hunterName = "Wesley";
+var nightsHunting = 8;
+var qtyStakes = 10;
+var typeProtection = "garlic";
+var readyToHunt;
+var huntingSites = ["graveyard", "cave", "monestary", "crypts"];
+var minutesSpentHunting = [120, 60, 240, 120];
+
+
+// Procedure
+if (nightsHunting >= 5) {
+	console.log (hunterName + " is a vampire hunting pro, and can hunt on his own.")
+}
+else {
+	console.log (hunterName + " is still new at vampire hunting and should go with a guide.")
+};
+
+// Boolean Function
+
+if (qtyStakes >=5 && typeProtection === "crucifix") {
+	console.log (hunterName + ", you are ready to go vampire hunting!")
+	readyToHunt = true;
+}
+else {
+	console.log (hunterName + ", you are not prepared to go vampire hunting!")
+	readyToHunt = false;
+}
+
+console.log (readyToHunt);
+
+// Number Function
+
+// String Function
+
+// Array Function
+
+//Returned Values
+
+//Output
