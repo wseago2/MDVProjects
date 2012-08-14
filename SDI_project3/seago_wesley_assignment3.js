62fcfbedbec6d9e2b579d1fcb7107ae15038984f
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-13 22:46:57 -0400

    JSON setup.

--- SDI_project3/seago_wesley_assignment3.js
+++ SDI_project3/seago_wesley_assignment3.js
@@ -1,1 +1,104 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
+// Wesley Seago
+// SDI term 1208
+// Project 3
+// 8/14/2012
+
+// local variables
+// returned values
+// output
+// conditional
+// nested conditional
+// while loop
+// for loop
+// math
+// back
+// argument: boolean
+// argument: array
+// argument: number
+// argument: string
+// argument: object
+
+
+// declare global variables
+
+
+// json data
+var hunters = { "killers"  : [
+								{ "playerId"   : "0001",
+								  "firstName"  : "Wes",
+								  "lastName"   : "Seago",
+								  "hunts"      : 11,
+								  "kills"      : 5
+								},
+
+								{ "playerId"   : "0002",
+								  "firstName"  : "Mark",
+								  "lastName"   : "Harris",
+								  "hunts"      : 7,
+								  "kills"      : 3
+								}
+                             ],
+
+                "support"  : [
+                				{ "playerId"   : "0003",
+                				  "firstName"  : "Brian",
+                				  "lastName"   : "Abrams",
+                				  "hunts"      : 12,
+                				  "kills"      : 7
+                				},
+
+                				{ "playerId"   : "0004",
+                				  "firstName"  : "Joe",
+                				  "lastName"   : "Green",
+                				  "hunts"      : 4,
+                				  "kills"      : 1
+                				}
+                 			 ],
+
+                "guides"    : [
+                				{ "playerId"   : "0005",
+                				  "firstName"  : "Hunchback",
+                				  "lastName"   : "Lou",
+                				  "hunts"      : 107,
+                				  "kills"      : 73
+                				},
+
+                				{ "playerId"   : "0006",
+                				  "firstName"  : "Riff",
+                				  "lastName"   : "Raff",
+                				  "hunts"      : 88,
+                				  "kills"	   : 71
+                				}
+                			  ]
+               };
+// check to see if json works
+console.log (hunters.guides[1].firstName);
+
+// property: boolean (return boolean)
+
+
+// property: array (return array)
+
+
+// property: number (return number)
+
+
+// property: string (return string)
+
+
+// property object (return object)
+
+
+// method: procedure
+
+
+// method: function
+
+
+// method: accessor
+
+
+// method: mutator
+
+
