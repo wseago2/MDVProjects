57eb9925438e6f57df5ed977471c9517fed835b0
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-16 09:55:16 -0400

    Added several elements.

--- SDI_project3/seago_wesley_assignment3.js
+++ SDI_project3/seago_wesley_assignment3.js
@@ -36,11 +36,11 @@ var json = {
 
 // Global Variables
 var stakesNeeded = 10;
-var qtyStakes;
 var availableSites = [json.huntingSites["0001"].name, json.huntingSites["0002"].name, json.huntingSites["0003"].name];
 // console.log (availableSites); test JSON access.
 var hunterLocation;
-
+var guides = ["Hunchback Lou", "Riff Raff", "Eyegore"];
+// console.log (availableSites); test JSON access.
 
 var wes = {
 	name:        "Wes",
@@ -53,18 +53,40 @@ var wes = {
 		wes.location = destination
 	},
 	getLocation: function (location) {
-		console.log (wes.location);
+		console.log ("I am currently at the " + wes.location);
 	}
 };
+
+
 
 // console.log (wes); // check object.
 // console.log (wes.location); // ahow starting location.
 // wes.travelTo (json.huntingSites["0003"].site); // method procedure.
 // console.log (wes.location); // check does travelTo method work.
 
-wes.getLocation (wes.location);
+// wes.getLocation (wes.location); // check does getLocation method work.
+console.log ("Our newest vampire hunter is " + wes.name + ".");
+console.log ("Before we can go vampire hunting, we must prepare.");
+console.log ("When hunting in a new site, we must be sure to bring " + stakesNeeded + " stakes with us.");
+
+if ( wes.qtyStakes >= stakesNeeded ) {
+	console.log ( wes.name + " has enough stakes and is prepared to hunt.");
+	} else { console.log ( wes.name + " is short " + (stakesNeeded - wes.qtyStakes) + " stakes, and needs to prepare for the next hunt." );
+};
+
+while ( wes.qtyStakes < stakesNeeded ) {
+	console.log ("Making a stake.");
+	wes.qtyStakes++;
+	console.log ("Wes now has " + wes.qtyStakes + " stakes.");
+}
+
+if ( wes.needGuide == true ) {
+	console.log ( "Since " + wes.name + " is new at vampire hunting, we will send him with a guide.");
+	} else { console.log ( wes.name + " has enough experience to hunt alone now." );
+};
 
 
+console.log ("Now that we are prepared to hunt, we will head over to the first hunting site.");
 
 
 
