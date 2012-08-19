9b637ab120f8727d0e3c8aaa0baf382ae3314a35
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-19 17:55:04 -0400

    Ready to code.

--- SDI_project4/seago_wesley_assignment4.js
+++ SDI_project4/seago_wesley_assignment4.js
@@ -2,4 +2,92 @@
 // SDI Term 1208
 // Assignment Project 4
 // Javascript Library
-alert("JavaScript works!");
+
+
+// String Manipulation Library
+
+var phoneNumberPattern function () {
+	// code here
+	return { };
+}
+
+
+
+var emailPattern function () {
+	//code here
+	return { };
+}
+
+
+
+var URLPattern function () {
+	//code here
+	return { };
+}
+
+
+
+var titleCaseString function () {
+	//code here
+	return { };
+}
+
+
+
+var stringChangeSeparator function () {
+	//code here
+	return { };
+}
+
+
+
+// Number Manipulation Library
+
+var formatDecimalPlaces function () {
+	//code here
+	return { };
+}
+
+
+
+var fuzzyMatch function () {
+	//code here
+	return { };
+}
+
+
+
+var hoursDaysBetween function () {
+	//code here
+	return { };
+}
+
+
+
+var stringToNumber function () {
+	//code here
+	return { };
+}
+
+
+
+// Array Manipulation Library
+
+var smallestGreaterThan function () {
+	//code here
+	return { };
+}
+
+
+
+var totalNumberValue function () {
+	//code here
+	return { };
+}
+
+
+
+var arraySortedByKeyValue function () {
+	//code here
+	return { };
+}
