c145e39839f752b61750e6ff356e5e1e9e67742b
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-21 11:43:08 -0400

    4 working.

--- SDI_project4/seago_wesley_assignment4.js
+++ SDI_project4/seago_wesley_assignment4.js
@@ -3,7 +3,13 @@
 // Assignment Project 4
 // Javascript Library
 
+/*
+var myLibrary = function () {
+	// code here
 
+	return { };	
+};
+*/
 
 // String Manipulation Library
 
@@ -12,100 +18,89 @@ function phoneNumberPattern (input) {
 	return(re.test(input));
 };
 
-
-
 function emailPattern (input) {
 	// code here ref textbook pg 191-202.
 	// regular expressions.
 	// setup regular expression params.
 	// comment out if/else statement if only need boolean value.
 	// same for other string regexp functions.
-	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // MAKEUP: proper use of escape character, project 1.
+		var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
 		//	if (re.test(input)===true) {
 		//	console.log (input + " follows a pattern like an email address.");
 		//} else {
 		//	console.log (input + " does not follow a pattern like an email address.");
 		//}
-		return(re.test(input)); // MAKEUP: missing output in boolean function, project 2.
+		return(re.test(input)); 
 };
-
-
 
 function urlPattern (input) {
 	var re = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 	return (re.test(input));
 };
 
+function titleCaseString (input) {
+	return input.replace(/\w\S*/g, function(change){return change.charAt(0).toUpperCase() + change.substr(1).toLowerCase();});
+	// 
+};
 
-
-function titleCaseString () {
-	//code here
-	return { };
-}
-
-
-
-function stringChangeSeparator () {
-	//code here
-	return { };
-}
+function stringChangeSeparator (input) {
+	var text = input.replace (/[,-]/g,"/"); // /=begin re;[what to change (, or -)]; /g= change all; "/" what to change to.
+	return (text);
+};
 
 
 
 // Number Manipulation Library
-
-function formatDecimalPlaces () {
-	//code here
+/*
+function formatDecimalPlaces (input) {
+	// code here
 	return { };
 }
+*/
 
-
-
+/*
 function fuzzyMatch () {
 	//code here
 	return { };
 }
+*/
 
-
-
+/*
 function hoursDaysBetween () {
 	//code here
 	return { };
 }
+*/
 
-
-
+/*
 function stringToNumber () {
 	//code here
 	return { };
 }
+*/
 
-
-
+/*
 // Array Manipulation Library
 
 function smallestGreaterThan () {
 	//code here
 	return { };
 }
+*/
 
-
-
+/*
 function totalNumberValue () {
 	//code here
 	return { };
 }
+*/
 
-
-
+/*
 function arraySortedByKeyValue () {
 	//code here
 	return { };
 };
-
-
-
-
+*/
 
 // Test phoneNumberPattern function.
 console.log (phoneNumberPattern("1234567890"));
@@ -118,3 +113,10 @@ console.log (emailPattern("wseago"));
 // Test urlPattern
 console.log (urlPattern("http://www.google.com"))
 console.log (urlPattern("google.com"));
+
+//Test titleCaseString function.
+console.log (titleCaseString("wesley g seago"));
+
+// Test stringChangeSeparator function.
+console.log (stringChangeSeparator("3,2,1"));
+console.log (stringChangeSeparator("3-2-1"));
