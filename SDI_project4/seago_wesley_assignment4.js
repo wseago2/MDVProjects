0c520d4cd40cd1b70779466eaa99bb49c60e5ef2
Wesley Seago <wseago2@Wesleys-MacBook-Pro.local>
2012-08-21 08:40:45 -0400

    Three done.

--- SDI_project4/seago_wesley_assignment4.js
+++ SDI_project4/seago_wesley_assignment4.js
@@ -4,32 +4,37 @@
 // Javascript Library
 
 
+
 // String Manipulation Library
 
 function phoneNumberPattern (input) {
-	// code here
-	return { };
-}
+	var re = /^\(?(\d{3})\)?[\.\-\/]?(\d{3})[\.\-\/]?(\d{4})$/;
+	return(re.test(input));
+};
 
 
 
 function emailPattern (input) {
-	// code here ref textbook pg 191-202
-	// regular expressions
-	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	return re.test(input);
-}
+	// code here ref textbook pg 191-202.
+	// regular expressions.
+	// setup regular expression params.
+	// comment out if/else statement if only need boolean value.
+	// same for other string regexp functions.
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // MAKEUP: proper use of escape character, project 1.
+		//	if (re.test(input)===true) {
+		//	console.log (input + " follows a pattern like an email address.");
+		//} else {
+		//	console.log (input + " does not follow a pattern like an email address.");
+		//}
+		return(re.test(input)); // MAKEUP: missing output in boolean function, project 2.
+};
 
-console.log (emailPattern("wseago"));
-console.log (emailPattern("wseago2@gmail.com"));
 
 
-
-
-function URLPattern () {
-	//code here
-	return { };
-}
+function urlPattern (input) {
+	var re = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	return (re.test(input));
+};
 
 
 
@@ -96,4 +101,20 @@ function totalNumberValue () {
 function arraySortedByKeyValue () {
 	//code here
 	return { };
-}
+};
+
+
+
+
+
+// Test phoneNumberPattern function.
+console.log (phoneNumberPattern("1234567890"));
+console.log (phoneNumberPattern("123456789"));
+
+// Test emailPattern function.
+console.log (emailPattern("wseago2@gmail.com"));
+console.log (emailPattern("wseago"));
+
+// Test urlPattern
+console.log (urlPattern("http://www.google.com"))
+console.log (urlPattern("google.com"));
