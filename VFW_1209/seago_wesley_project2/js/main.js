b77b499c6f5db18abf0433f32b2d9149f72c20d1
Wesley Seago <wseago2@fullsail.edu>
2012-09-04 22:02:15 -0400

    CSS Done.

---
+++ VFW_1209/seago_wesley_project2/js/main.js
@@ -0,0 +1,50 @@
+// Wesley Seago
+// VFW Term 1209
+// Project 2 
+// Medicine Tracker
+
+// Wait until the DOM is ready
+//window.addEventListener("DOMContentLoaded", function (){
+
+
+
+	// Variable Defaults
+
+
+
+
+	// function to save data. Submit button id=saveData.
+/*
+	function storeData(){
+		var id = Math.floor(Math.random()*100000001);
+		var item = {};
+			item.name 		= ["Name", $('name').value];
+			item.medname	= ["Medication Name", $('name').value];
+			item.date 		= ["Date", $('name').value];
+			item.notes 		= ["Notes", $('name').value];
+			// Save Data into local storage.
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Medication Saved!");
+	};
+	
+		
+
+	// function to clear data. Link id = clearData.
+	
+
+	// function to display data. Display data link id = displayData.
+	
+
+	// Set link & submit click events
+	//var displayLink=document.getElementById(displayData);
+	//displayLink.addEventListener("click", displayData);
+
+	//var clearLink = $('clearData');
+	//clearLink.addEventListener("click", clearData);
+
+	//var save = $('saveData');
+	
+	
+
+});
+*/
