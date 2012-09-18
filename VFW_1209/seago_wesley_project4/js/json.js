e83cd721933928f530b7b05c9adb15f502d819d7
Wesley Seago <wseago2@fullsail.edu>
2012-09-18 07:29:44 -0400

    Setup Project 4. Added JSON default data.

---
+++ VFW_1209/seago_wesley_project4/js/json.js
@@ -0,0 +1,33 @@
+// Wesley Seago
+// VFW Term 1209
+// Project 4 
+// Medicine Tracker
+
+var json = {
+	"medication1": {
+		"name": ["Name:", "Chloe"],
+		"medname": ["Medication Name:", "Tylenol"],
+		"typename": ["Type:", "Over the Counter"],
+		"dosage": ["Dosage:", "1"],
+		"frequency": ["Frequency:", "4 Hours"],
+		"date": ["Date:", "2012-9-18"],
+		"notes": ["Notes:", "Fever"]
+	},
+	"medication2": {
+		"name": ["Name:", "Erin"],
+		"medname": ["Medication Name:", "Tylenol"],
+		"typename": ["Type:", "Over the Counter"],
+		"dosage": ["Dosage:", "5"],
+		"frequency": ["Frequency:", "4 Hours"],
+		"date": ["Date:", "2012-9-18"],
+		"notes": ["Notes:", "Fever"]
+	},"medication3": {
+		"name": ["Name:", "Wes"],
+		"medname": ["Medication Name:", "Tylenol"],
+		"typename": ["Type:", "Over the Counter"],
+		"dosage": ["Dosage:", "5"],
+		"frequency": ["Frequency:", "4 Hours"],
+		"date": ["Date:", "2012-9-18"],
+		"notes": ["Notes:", "Fever"]
+	}
+}
