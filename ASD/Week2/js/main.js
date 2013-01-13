e98a77e5627330f706328146cf55644d54b571a4
Wesley Seago <wseago2@fullsail.edu>
2013-01-13 16:53:09 -0500

    Key issue resolved, Delete function complete.

--- ASD/Week2/js/main.js
+++ ASD/Week2/js/main.js
@@ -6,7 +6,7 @@
 //home Page
 //###########################################################################################
 $('#home').on('pageinit', function(){
-	alert("The home page was initialized");
+	
 });
 
 //###########################################################################################
@@ -21,49 +21,13 @@ $('#addItem').on('pageinit', function(){
 					var data = myForm.serializeArray();
 					storeData(data);
 				}
-		});
-});
+		});	
 
-//###########################################################################################
-//displayItems Page
-//###########################################################################################
-$('#displayItems').on('pageinit', function(){
-		
-});
-
-//###########################################################################################
-// displayLink Button
-//###########################################################################################
-$('#displayLink').on("click", function(){
-		displayEvents();
-});
-
-//###########################################################################################
-// Clear Local Storage Button
-//###########################################################################################
-$("#clear").on("click", function(){
-		if(localStorage.length === 0){
-		alert("There is no data to clear.");
-		}else{
-			localStorage.clear();
-			alert("All data has been cleared!");
-				}				
-});
-
-//###########################################################################################
-// autoFillData function
-//###########################################################################################
-var autoFillData = function(){
-	 		for (var n in json){
-				var id = Math.floor(Math.random()*100000001);
-				localStorage.setItem(id, JSON.stringify(json[n]));
-				}
-};	
-
-//###########################################################################################
-// storeData function
-//###########################################################################################
-var storeData = function(key){
+		//###########################################################################################
+		// storeData function
+		//###########################################################################################
+		var storeData = function(key){
+			alert(key);
 //Using existing key even when a new key should be assigned.
 //This is a result of the key being wrapped in jQuery and turned into an object.
 //Gotta get this solved to move forward with edit and delete links.
@@ -85,51 +49,76 @@ var storeData = function(key){
 					item.notes	 		=["Notes: ", $("#notes").val()];
 						localStorage.setItem(id, JSON.stringify(item));
 						alert("Information Saved!");
-//						window.location.reload();
-						$.mobile.changePage("#home");
+						window.location.reload();
+		};
 
+		//####################################################################################
+		// displayLink Button
+		//####################################################################################
+		$('#displayLink').on("click", function(){
+			displayEvents();
+		});
 
-};
+		//####################################################################################
+		// Clear Local Storage Button
+		//####################################################################################
+		$("#clear").on("click", function(){
+			if(localStorage.length === 0){
+				alert("There is no data to clear.");
+			}else{
+				localStorage.clear();
+				alert("All data has been cleared!");
+				}				
+		});
+});
 
 //###########################################################################################
+//loadData Page
+//###########################################################################################
+$('#loadData').on('pageinit', function(){
+	
+});
+
+//###########################################################################################
+//displayItems Page
+//###########################################################################################
+$('#displayItems').on('pageinit', function(){
+			
+});
+
+//###################################################################################
 // displayEvents function
-//###########################################################################################
-var displayEvents = function (){
-	$('.events').remove();
-	alert("The events list was cleared!");
-	if(localStorage.length === 0){
-		alert("There is no Data in Local Storage, so Default Data was Added.");
-		autoFillData();
-	}else{
-		alert("Local Storage Contains Data");
-		$.mobile.changePage("#displayItems");
-		var makeEventList = $('<ul>');
-			//give the ul an id so I can clear it without clearing the entire page.
-			//if I use .empty on the page I will lose navigation.
-			//I would really like to keep the persistent, consistent navigation.
-			makeEventList.attr("id", "events");
-			makeEventList.attr("class", "events");
-			makeEventList.appendTo("#eventDisplay");
-				for (var i=0, len=localStorage.length; i<len; i++){
-					var makeEventRecord = $('<li>');
-					var lineBreak = $('<br><br>');
-					makeEventRecord.appendTo(makeEventList);
-					var key = localStorage.key(i);
-//					var key = $("localStorage.key(i)").html(val);
-					var value = localStorage.getItem(key);
-					var obj = JSON.parse(value);
-						var makeSubList = $('<ul>');
-						makeSubList.appendTo(makeEventRecord);
+//###################################################################################
+		var displayEvents = function (){
+			$('.events').remove();
+			if(localStorage.length === 0){
+				alert("There is no Data in Local Storage, so Default Data was Added.");
+				autoFillData();
+			}else{
+				alert("Local Storage Contains Data");
+				$.mobile.changePage("#displayItems");
+				var makeEventList = $('<ul>');
+					makeEventList.attr("id", "events");
+					makeEventList.attr("class", "events");
+					makeEventList.appendTo("#eventDisplay");
+						for (var i=0, len=localStorage.length; i<len; i++){
+							var makeEventRecord = $('<li>');
+							var lineBreak = $('<br><br>');
+							makeEventRecord.appendTo(makeEventList);
+							var key = localStorage.key(i);
+							var value = localStorage.getItem(key);
+							var obj = JSON.parse(value);
+							var makeSubList = $('<ul>');
+							makeSubList.appendTo(makeEventRecord);
 							var editLink = $("<button>");
-							var editText = "Edit Event";
+							var editText = "Edit This Event";
 							var deleteLink = $("<button>");
-							var deleteText = "Delete Event";
+								deleteLink.attr("key", key);
+							var deleteText = "Delete This Event";
 							editLink.attr("href", "#addItem");
 							deleteLink.attr("href", "#displayItems");
 							editLink.text(editText);
 							deleteLink.text(deleteText);
-								editLink.on("click", editItem);
-								deleteLink.on("click", deleteItem);
 							editLink.appendTo(makeEventRecord);
 							deleteLink.appendTo(makeEventRecord);
 							lineBreak.appendTo(makeEventRecord);
@@ -138,29 +127,40 @@ var displayEvents = function (){
 									makeSubLi.appendTo(makeSubList);
 									var optSubText = obj[n][0]+" "+obj[n][1];
 									makeSubLi.text(optSubText);
-									}				
-					}
-			}
-
-};
+									}
+								editLink.on("click", editItem);
+								deleteLink.on("click", deleteItem);				
+							}
+				}			
+			};
 
 //###########################################################################################
+// autoFillData function
+//###########################################################################################
+		var autoFillData = function(){
+	 		for (var n in json){
+				var id = Math.floor(Math.random()*100000001);
+				localStorage.setItem(id, JSON.stringify(json[n]));
+				}
+		};
+
+//###################################################################################
 // editItem function
-//###########################################################################################
-function editItem(){
-			alert("The editItem function has fired.");
+//###################################################################################
+		function editItem(){
+			var currentKey = $(this).attr("key");
 			var value = localStorage.getItem(this.key);
-//			var value = this.getItem(key);
+//		    var value = deleteLink.this.key;
 			alert(value);
-			var item = JSON.parse(value);
-			alert(item);
+//			var item = JSON.parse(value);
+//			alert(item);
 //			var value = localStorage.getItem(this.key);
 //			var thiskey = $(this).attr("key");
 //			var value = localStorage.getItem($(this).attr("key"));
 //			var item = JSON.parse(value);
 				//Populate the form fields with current local storage values.
-		 	$("name").val(item.name[1]);
-			$("medname").val = (item.medname[1]);
+//		 	$("name").val(item.name[1]);
+//			$("medname").val = (item.medname[1]);
 			//Take out radios until fixed.
 			//	var radios = document.forms[0].type;
 			//	for (var i=0; i<radios.length; i++){
@@ -170,35 +170,37 @@ function editItem(){
 			//			radios[i].setAttribute("checked", "checked");
 			//		}
 			//	}
-			$("dosage").val = (item.dosage[1]);
-			$("frequency").val = (item.frequency[1]);
-			$("date").val = (item.date[1]);
-			$("notes").val = (item.notes[1]);
+//			$("dosage").val = (item.dosage[1]);
+//			$("frequency").val = (item.frequency[1]);
+//			$("date").val = (item.date[1]);
+//			$("notes").val = (item.notes[1]);
 		
-			var editSubmit = $("submit")
-			$("submit").val("Edit Event");
-		//Save key value established in this function as a property of the editSubmit event
-		//so we can use that value when we save the data that we edited.
-				editSubmit.on("click", storeData);
-				editSubmit.key = this.key;
-				return;
-};
+//			var editSubmit = $("submit")
+//			$("submit").val("Edit Event");
+			//Save key value established in this function as a property of the editSubmit event
+			//so we can use that value when we save the data that we edited.
+//				editSubmit.on("click", storeData);
+//				editSubmit.key = this.key;
+//				return;
+			};
 
-
-//###########################################################################################
+//###################################################################################
 // deleteItem function
-//###########################################################################################
-function deleteItem(){
-	var ask = confirm("Are you sure you want to delete this event?");
-		if (ask){
-			localStorage.removeItem(this.key);
-			alert("Event Deleted.");
-			$.mobile.changePage("#addItem");
-		}else{
-			alert("Event was NOT Deleted.");
-		}
-		return false;
-};
+//###################################################################################
+	var deleteItem = function(){
+			var currentKey = $(this).attr("key");
+			var ask = confirm("Are you sure you want to delete this event?");
+				if (ask){
+					localStorage.removeItem(currentKey);
+					alert("Event Deleted.");
+					$.mobile.changePage("#addItem");
+				}else{
+					alert("Event was NOT Deleted.");
+			}
+		};
+
+
+
 
 
 
